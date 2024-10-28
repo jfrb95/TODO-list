59294481748a2aeb9b4eb859503bc1d90b0675d4
Jako <43934830+jfrb95@users.noreply.github.com>
2024-10-28 17:49:21 +0000

    begin index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 import "./style.css";
 
+const log = console.log;
+
+const GLOBAL = (function() {
+    const container = document.querySelector(".container");
+    const navigationBar = document.querySelector(".navigation");
+    const contentPanel = document.querySelector(".content");
+
+    const utilities = function() {
+        function readData(path) {
+            return require(`${path}`);
+        }
+
+        return {
+            readData,
+        }
+    }();
+
+    const data = utilities.readData("./data.json");
+    
+    function writeNewTask(name, project, dateCreated, deadline, description, tags, priority) {
+        
+    }
+
+})();
