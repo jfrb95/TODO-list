b36dbea484557238cf16feabc0867d86d4e3cd16
Jako <43934830+jfrb95@users.noreply.github.com>
2024-10-29 07:33:36 +0000

    add internal data storage and write function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import "./style.css";
 const log = console.log;
 
 const GLOBAL = (function() {
+    const dataPath = "./data.json";
+
     const container = document.querySelector(".container");
     const navigationBar = document.querySelector(".navigation");
     const contentPanel = document.querySelector(".content");
@@ -12,15 +14,63 @@ const GLOBAL = (function() {
             return require(`${path}`);
         }
 
+        function writeNewTask(name, project, dateCreated, deadline, description, tags, priority) {
+            const newTask = {
+                name,
+                project,
+                dateCreated,
+                deadline,
+                description,
+                tags,
+                priority
+            }
+    
+            addTaskToData(newTask, data);
+        }
+    
+        function addTaskToData(task, data) {
+            data.push(task);
+        }
+
         return {
             readData,
+            writeNewTask,
+            addTaskToData
         }
     }();
 
-    const data = utilities.readData("./data.json");
-    
-    function writeNewTask(name, project, dateCreated, deadline, description, tags, priority) {
-        
-    }
+    const data = [
+        {
+            name: "task1",
+            project: "project1",
+            dateCreated: "date1",
+            deadline: "date2",
+            description: "user-created description of task1",
+            tags: [
+                "wedding",
+                "funny"
+            ],
+            priority: 1
+        },
+
+        {
+            name: "task2",
+            project: "project1",
+            dateCreated: "date3",
+            deadline: "date4",
+            description: "user-created description of task2",
+            tags: [
+                "dog",
+                "american"
+            ],
+            priority: 5
+        }
+    ];
+
+
+
+    utilities.writeNewTask("task3", "project2", "date5", "date6", "description of task3", ["tag1", "tag2"], 3);
+
+    log(data);
 
 })();
